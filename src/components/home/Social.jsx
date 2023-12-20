8272e9a23312e5a2a2cc5be5ff71faaeecc314e5
Omkar Pedamkar <omkarpedamkar@Omkars-MacBook-Air.local>
2023-12-20 17:06:48 +0530

    complete home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+        
+        <a href="https://www.instagram.com/omkar_pedamkar13" className='home__social-icon' target='_blank' rel="noreferrer">
+            <i class="uil uil-instagram-alt"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/omkar-pedamkar/" className='home__social-icon' target='_blank' rel="noreferrer">
+            <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/omkar1304" className='home__social-icon' target='_blank' rel="noreferrer">
+            <i class="uil uil-github"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
