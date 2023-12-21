d9b5ced22eb728d2057e4140f5d2f0a4b5be7d00
Omkar Pedamkar <omkarpedamkar@Omkars-MacBook-Air.local>
2023-12-21 15:01:43 +0530

    completed about section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='about__info grid'>
+        <div className='about__box'>
+            <i class='bx bx-award about__icon'></i>
+            <h3 className='about__title'>Experience</h3>
+            <span>8 Years Working</span>
+        </div>
+
+        <div className='about__box'>
+            <i class='bx bxs-briefcase-alt about__icon'></i>
+            <h3 className='about__title'>Completed</h3>
+            <span>48+ Projects</span>
+        </div>
+
+        <div className='about__box'>
+            <i class='bx bx-support about__icon'></i>
+            <h3 className='about__title'>Support</h3>
+            <span>Online 24/7</span>
+        </div>
+    </div>
+  )
+}
+
+export default Info
